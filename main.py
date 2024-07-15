8585f266e2bd17cd75e8789d4b5ca61965983269
Monae <93678860+purplemo340@users.noreply.github.com>
2024-07-15 12:19:28 -0400

    instead of destroying the buttons, the placements on the grid was forgotten. Now the loop continues

--- main.py
+++ main.py
@@ -6,25 +6,32 @@ global player
 global game_arr
 global arr
 global buttons
-
-window=Tk()
+global congrats
+buttons=[]
+window = Tk()
 window.title('Tic Tac Toe')
 window.config(padx=50, pady=50, height=200, width=200)
 
-arr=['X', 'O']
-score_dict={'X':0, 'O':0}
+arr = ['X', 'O']
+score_dict = {'X': 0, 'O': 0}
 player = 0
+
+
 def game():
     global game_arr
-    buttons_arr=[]
+    global buttons
+    buttons = []
+
     game_arr = board()
+
+    score.grid(row=3, column=0, columnspan=3)
     for x in range(9):
-        button=Button(height=0, width=5, font=24, command= lambda place=x:switch(place))
-        buttons_arr.append(button)
-        buttons_arr[x].grid(column=int(x/3), row=x%3)
-    return buttons_arr
+        button = Button(window, height=2,  width=5, font=24, command=lambda place=x: switch(place))
+        buttons.append(button)
+        buttons[x].grid(column=int(x/3), row=x % 3)
+    return buttons
 def board():
-    array = [[str(x*3), str((x*3)+1), str((x*3)+2)] for x in range(0,3)]
+    array = [[str(x*3), str((x*3)+1), str((x*3)+2)] for x in range(0, 3)]
 
     return array
 
@@ -54,40 +61,55 @@ def switch(x):
 
         buttons[x].config(text=f"{arr[player]}")
         winner = win(game_arr)
-        if winner==True:
+        if winner:
             score_dict['X'] += 1
             end(player)
 
-        player=1
+        player = 1
     else:
         game_arr[int(x / 3)][x % 3] = f'{arr[player]}'
 
         buttons[x].config(text=f"{arr[player]}")
-        winner=win(game_arr)
-        if winner==True:
+        winner = win(game_arr)
+        if winner:
             score_dict['O'] += 1
+
             end(player)
-        player=0
+        player = 0
+
+
 def end(winner):
+    global buttons
+    global congrats
     score.config(text=f"X: {score_dict['X']}\t O: {score_dict['O']}")
 
     for button in buttons:
-        button.destroy()
+        button.config(text="")
+        button.grid_forget()
 
-    congrats.config(text=f"Congrats!: {arr[winner]}")
-    congrats.grid(column=0,row=0)
+
+    congrats=Label(text=f"Congrats!: {arr[winner]}")
+    congrats.grid(column=0, row=0)
 
     con.grid(column=0, row=1)
 def next_game():
+    global congrats
     global buttons
-    con.destroy()
-    congrats.destroy()
-    buttons=game()
+    global game_arr
+
+    game_arr=board()
+    con.grid_forget()
+    congrats.grid_forget()
+    for x in range(9):
+
+        buttons[x].grid(column=int(x/3), row=x % 3)
+
 
 score = Label(text=f"X: {score_dict['X']}\t O: {score_dict['O']}")
-score.grid(row=3, column=0, columnspan=3)
 
-buttons=game()
+
+buttons = game()
 con=Button(text="Continue", command=next_game)
-congrats=Label()
+
+
 window.mainloop()
